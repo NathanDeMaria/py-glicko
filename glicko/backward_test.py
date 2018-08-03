3ac37df80a97f3755829706f0765a4186edcb393
Nathan DeMaria <nathandmaria@gmail.com>
2018-08-02 21:58:47 -0500

    Test better

--- glicko/backward_test.py
+++ glicko/backward_test.py
@@ -15,7 +15,6 @@ def _get_team(name: str, teams: List[Team]) -> Team:
     return matches[0]
 
 
-# TODO: better unit testable functions in backward.py
 class TestBackward(TestCase):
     def test_backward__no_other_opponents(self):
         # If a team has only one opponent in a season, don't break.
@@ -89,3 +88,22 @@ class TestBackward(TestCase):
 
         a2 = _get_team('a', teams)
         self.assertGreater(a2.rating, a.rating)
+
+    def test_backward__none_from_next_season(self):
+        # You don't get a boost from beating
+        # somebody with a big win next season
+        league = League([
+            Game('a', 'b', 1, 0, 1, 1, datetime.now(), 'A'),
+        ])
+        _, teams = run_league(league, 100)
+
+        a = _get_team('a', teams)
+
+        league2 = League([
+            Game('a', 'b', 1, 0, 1, 1, datetime(1976, 3, 24), 'A'),
+            Game('b', 'c', 1, 0, 2, 1, datetime.now(), 'A'),
+        ])
+        _, teams = run_league(league2, 100)
+
+        a2 = _get_team('a', teams)
+        self.assertEqual(a2.rating, a.rating)
