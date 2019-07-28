2ff91efc42a4171ed68cef6c01b4674a9ac29749
Nathan DeMaria <nathandmaria@gmail.com>
2019-07-28 12:48:22 -0500

    Handle new groups and first season init

--- glicko/offseason.py
+++ glicko/offseason.py
@@ -38,27 +38,60 @@ def create_grouped_offseason_runner(
     """
     def run_offseason(league: League, season: int) -> None:
         rating_lookup = _create_rating_lookup(league, season)
+
+        # Hack: b/c seasons aren't necessarily zero indexed
+        first_season = min(g.season for g in league.games)
+
         for team in league.teams:
-            try:
-                mean, var = team.rating
-                team.update_rating(
-                    (mean, var + variance_over_time),
-                    season,
-                    0
-                )
-            except ValueError:
-                # There's no rating for this team yet.
-                group = team.get_group(season)
-                if group is None:
-                    # If the team didn't play in this season,
-                    # we don't need to give them a rating yet.
-                    continue
-                group_rating = rating_lookup.get(group)
-                if group_rating is None:
-                    # TODO: handle if a group of totally new teams appears
-                    raise NotImplementedError()
-                team.update_rating((group_rating, init_variance), season, 0)
+            if season == first_season and team.get_group(season) is not None:
+                # It's the first season,
+                # and this team played in the first season
+                team.update_rating((1500., init_variance), season, 0)
+            else:
+                try:
+                    mean, var = team.rating
+                    team.update_rating(
+                        (mean, var + variance_over_time),
+                        season,
+                        0
+                    )
+                except ValueError:
+                    # There's no rating for this team yet.
+                    group = team.get_group(season)
+                    if group is None:
+                        # If the team didn't play in this season,
+                        # we don't need to give them a rating yet.
+                        continue
+                    group_rating = rating_lookup.get(group)
+                    if group_rating is None:
+                        group_rating = _get_new_group_rating(rating_lookup, group)
+                    team.update_rating((group_rating, init_variance), season, 0)
     return run_offseason
+
+
+def _get_new_group_rating(rating_lookup: Dict[str, float], group: str) -> float:
+    """
+    Assuming the group names are in some relevant lexicographical order,
+    grab the ratings of the nearest groups alphabetically.
+
+    If it's between two groups, take the average.
+    If it's off either end of the list, take the closest.
+    """
+    sorted_ratings = sorted(rating_lookup.items(), key=lambda kv: kv[0])
+    i = 0
+    for i, (g, _) in enumerate(sorted_ratings):
+        if g > group:
+            break
+    else:
+        # Runs if for executes without breaking, making i the index
+        # group would belong if inserted into the list of ratings
+        i += 1
+    
+    if i == 0:
+        return sorted_ratings[0][1]
+    if i >= len(sorted_ratings):
+        return sorted_ratings[-1][1]
+    return (sorted_ratings[i][1] + sorted_ratings[i - 1][1]) / 2
 
 
 def _create_rating_lookup(league: League, season: int) -> Dict[str, float]:
--- glicko/offseason_test.py
+++ glicko/offseason_test.py
@@ -1,5 +1,7 @@
+import datetime
 from unittest import TestCase
 
+from .game import Game
 from .league import League
 from .offseason import create_grouped_offseason_runner
 from .team import Team
@@ -15,8 +17,10 @@ class TestOffseason(TestCase):
             Team('t1', groups={0: 'A', 1: 'A'}),
             Team('t2', groups={1: 'A'})
         ]
+        games = [Game(teams[0], teams[1], 1, 2, 0, 0, datetime.datetime.now())]
+        league = League(teams=teams, games=games)
+        RUN_OFFSEASON_GROUPED(league, 0)
         teams[0].update_rating((team_mean, 4), 0, 1)
-        league = League(teams=teams, games=[])
 
         RUN_OFFSEASON_GROUPED(league, 1)
         # Make sure teams[1] takes the average from teams[0]
@@ -28,8 +32,10 @@ class TestOffseason(TestCase):
             Team('t1', groups={0: 'A', 1: 'A', 2: 'A'}),
             Team('t2', groups={2: 'A'})
         ]
+        games = [Game(teams[0], teams[1], 1, 2, 0, 0, datetime.datetime.now())]
+        league = League(teams=teams, games=games)
+        RUN_OFFSEASON_GROUPED(league, 0)
         teams[0].update_rating((team_mean, 4), 0, 1)
-        league = League(teams=teams, games=[])
 
         # Make sure teams[1] takes the average from teams[0]
         # but only after the 2nd offseason
@@ -39,3 +45,60 @@ class TestOffseason(TestCase):
         RUN_OFFSEASON_GROUPED(league, 2)
 
         self.assertEqual(teams[1].rating[0], team_mean)
+
+    def test_grouped_runner__new_group_middle(self):
+        team_mean = 123.
+        teams = [
+            Team('t1', groups={0: 'A', 1: 'A'}),
+            Team('t2', groups={1: 'B'}),
+            Team('t3', groups={0: 'C', 1: 'C'}),
+            Team('t4', groups={0: 'D', 1: 'D'}),
+        ]
+        games = [Game(teams[0], teams[1], 1, 2, 0, 0, datetime.datetime.now())]
+        league = League(teams=teams, games=games)
+        RUN_OFFSEASON_GROUPED(league, 0)
+        teams[0].update_rating((team_mean / 2, 4), 0, 1)
+        teams[2].update_rating((team_mean * 3 / 2, 4), 0, 1)
+        teams[3].update_rating((team_mean * 10, 4), 0, 1)
+
+        RUN_OFFSEASON_GROUPED(league, 1)
+        # Make sure teams[1] takes the average from teams[0] and teams[2]
+        self.assertEqual(teams[1].rating[0], team_mean)
+
+    def test_grouped_runner__new_group_start(self):
+        team_mean = 123.
+        teams = [
+            Team('t2', groups={1: 'A'}),
+            Team('t1', groups={0: 'B', 1: 'B'}),
+            Team('t3', groups={0: 'C', 1: 'C'}),
+            Team('t4', groups={0: 'D', 1: 'D'}),
+        ]
+        games = [Game(teams[0], teams[1], 1, 2, 0, 0, datetime.datetime.now())]
+        league = League(teams=teams, games=games)
+        RUN_OFFSEASON_GROUPED(league, 0)
+        teams[1].update_rating((team_mean, 4), 0, 1)
+        teams[2].update_rating((team_mean * 10, 4), 0, 1)
+        teams[3].update_rating((team_mean * 10, 4), 0, 1)
+
+        RUN_OFFSEASON_GROUPED(league, 1)
+        # Make sure teams[0] takes the average from teams[1]
+        self.assertEqual(teams[0].rating[0], team_mean)
+
+    def test_grouped_runner__new_group_end(self):
+        team_mean = 123.
+        teams = [
+            Team('t1', groups={0: 'A', 1: 'A'}),
+            Team('t4', groups={0: 'B', 1: 'B'}),
+            Team('t3', groups={0: 'C', 1: 'C'}),
+            Team('t2', groups={1: 'D'}),
+        ]
+        games = [Game(teams[0], teams[1], 1, 2, 0, 0, datetime.datetime.now())]
+        league = League(teams=teams, games=games)
+        RUN_OFFSEASON_GROUPED(league, 0)
+        teams[0].update_rating((team_mean * 10, 4), 0, 1)
+        teams[1].update_rating((team_mean * 10, 4), 0, 1)
+        teams[2].update_rating((team_mean, 4), 0, 1)
+
+        RUN_OFFSEASON_GROUPED(league, 1)
+        # Make sure teams[3] takes the average from teams[2]
+        self.assertEqual(teams[3].rating[0], team_mean)
