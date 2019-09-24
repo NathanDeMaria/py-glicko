a98deb5e2bad70f1c9c5bc9a1569747c8f9f9147
Nathan DeMaria <nathandmaria@gmail.com>
2019-09-24 18:57:59 -0500

    Add option to pass in all the games

--- glicko/league.py
+++ glicko/league.py
@@ -5,12 +5,19 @@ from .team import Team
 
 
 class League:
-    def __init__(self, games: List[Game], teams: List[Team] = None) -> None:
+    def __init__(self, games: List[Game], teams: List[Team] = None, home_only: bool = True) -> None:
+        """
+        :param games: list of games
+        :param teams: list of teams, will be pulled from teams on games if none are provided
+        :param home_only: leave as true if each game only shows up once.
+            Set to false if the games list already has the "flipped" games
+        """
         self.games = games
         reversed_games = []
-        for g in games:
-            reversed_game = g.flipped
-            reversed_games.append(reversed_game)
+        if home_only:
+            for g in games:
+                reversed_game = g.flipped
+                reversed_games.append(reversed_game)
         self.games += reversed_games
 
         if teams is None:
