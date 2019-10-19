44112573346c731e97cd841d60d4db95cf50c01d
Nathan DeMaria <nathandmaria@gmail.com>
2019-10-19 13:09:51 -0500

    Update NFL

--- app/leagues/nfl/nfl.py
+++ app/leagues/nfl/nfl.py
@@ -6,7 +6,7 @@ from glicko import LeagueBuilder, read_csv, Parameter
 LEAGUE_DIR = os.path.dirname(os.path.abspath(__file__))
 LEAGUE = 'nfl'
 CSV_PATH = os.path.join(LEAGUE_DIR, f'{LEAGUE}.csv')
-PARAM_PATH = os.path.join(LEAGUE_DIR, f'{LEAGUE}.json')
+PARAMS_FILE = os.path.join(LEAGUE_DIR, f'{LEAGUE}.json')
 
 league = read_csv(CSV_PATH)
 builder = LeagueBuilder(
@@ -14,20 +14,20 @@ builder = LeagueBuilder(
     [
         Parameter(
             name='init_variance',
-            value=20532.046258449554,
+            value=11314.210465759572,
             bounds=[1.0, 1e5],
         ),
         Parameter(
             name='variance_over_time',
-            value=18432.791531085968,
+            value=13438.058369333827,
             bounds=[1.0, 1e5]
         )
     ]
 )
-if os.path.exists(PARAM_PATH):
-    builder.load_parameters(PARAM_PATH)
+if os.path.exists(PARAMS_FILE):
+    builder.load_parameters(PARAMS_FILE)
 
 
 if __name__ == '__main__':
     print(builder.optimize()[0])
-    builder.save_parameters(PARAM_PATH)
+    builder.save_parameters(PARAMS_FILE)
